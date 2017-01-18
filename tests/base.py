2ed742a826ff3d77c31ce334fa86453be159185c
Nash <nashruddin.amin@gmail.com>
2017-01-18 19:52:51 +0700

    added more unit tests

--- tests/base.py
+++ tests/base.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 
 FIXTURES_DIR = join(dirname(abspath(__file__)), 'fixtures')
+ASSETS_DIR = join(dirname(abspath(__file__)), 'assets')
 
 class BaseTests(TestCase):
     fixtures = [
@@ -14,3 +15,9 @@ class BaseTests(TestCase):
     def setUp(self):
         self.bob = User.objects.get(username='bob')
         self.alice = User.objects.get(username='alice')
+        self.sample_image = open(join(ASSETS_DIR, 'sample_image.png'), 'rb')
+        self.sample_textfile = open(join(ASSETS_DIR, 'lorem.txt'), 'rb')
+
+    def tearDown(self):
+        self.sample_image.close()
+        self.sample_textfile.close()
--- tests/user/test_forms.py
+++ tests/user/test_forms.py
@@ -1,29 +1,85 @@
 import inspect
+
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-from apps.user.forms import ChangePasswordForm
-
+from apps.user.forms import UserProfileForm, ChangePasswordForm
 from ..base import BaseTests
 
 
+class UserProfileFormTests(BaseTests):
+    def test_form_should_require_user_parameter(self):
+        try:
+            form = UserProfileForm()
+            self.fail('should raise exception here')
+        except Exception: 
+            pass
+        try:
+            form = UserProfileForm(user=self.bob)
+        except Exception:
+            self.fail('Should not raise exception here')
+
+    def test_form_should_define_correct_fields(self):
+        fields = ['name', 'email', 'picture', 'bio', 'website']
+        form = UserProfileForm(user=self.bob)
+
+        self.assertEqual(len(form.fields), len(fields))
+        for field in fields:
+            self.assertTrue(field in form.fields)
+            self.assertIsNotNone(form.fields[field])
+
+    def test_form_should_set_instance_and_initial_email(self):
+        form = UserProfileForm(user=self.bob)
+
+        self.assertEqual(form.instance, self.bob.profile)
+        self.assertEqual(form.initial['email'], self.bob.email)
+
+    def test_with_invalid_data(self):
+        f = self.sample_textfile
+        form = UserProfileForm(
+            dict(email='xxx'),
+            dict(picture=SimpleUploadedFile(f.name, f.read())),
+            user=self.bob,
+        )
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['email'][0], 
+            'Enter a valid email address.')
+        self.assertEqual(form.errors['picture'][0],
+            'Upload a valid image. '
+            'The file you uploaded was either not an image or a corrupted image.')
+
+    def test_with_valid_data(self):
+        f = self.sample_image
+        form = UserProfileForm(
+            dict(email='user@example.com'),
+            dict(picture=SimpleUploadedFile(f.name, f.read())),
+            user=self.bob,
+        )
+
+        self.assertTrue(form.is_valid())
+
+
 class ChangePasswordFormTests(BaseTests):
+    def test_form_should_define_correct_fields(self):
+        fields = ['current_password', 'new_password', 'confirm_new_password']        
+        form = ChangePasswordForm(user=self.bob)
+
+        self.assertEqual(len(form.fields), len(fields))
+        for f in fields:
+            self.assertTrue(f in form.fields)
+            self.assertIsNotNone(form.fields[f])
+
     def test_form_should_require_user_parameter(self):
         try:
             form = ChangePasswordForm()
             self.fail('Should raise Exception here.')
-        except KeyError: 
+        except Exception: 
             pass
         try:
             form = ChangePasswordForm(user=self.bob)
         except KeyError:
             self.fail('Should not raise Exception here.')
-
-    def test_form_should_define_correct_fields(self):
-        form = ChangePasswordForm(user=self.bob)
-
-        for f in ['current_password', 'new_password', 'confirm_new_password']:
-            self.assertTrue(f in form.fields)
-            self.assertIsNotNone(form.fields[f])
 
     def test_with_invalid_data(self):
         data = dict(
