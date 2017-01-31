b6e1921d19fc3b3b9f092a9cf72c2cdc63edc705
Nash <nashruddin.amin@gmail.com>
2017-01-31 14:06:31 +0700

    added confirmation modal dialog

--- resources/static/js/app.js
+++ resources/static/js/app.js
@@ -1,3 +1,19 @@
+// Function for displaying confirmation modal
+function confirmModal(title, msg, callback) {
+  var dialog = $('#confirmModal');
+  dialog
+    .find('.modal-title').text(title).end()
+    .find('.modal-body').html(msg).end()
+    .find('.btn-ok').on('click', function() {
+      $(this).off('click');
+      dialog.modal('hide');
+      if (typeof(callback) === 'function') {
+        callback();
+      }
+    }).end()
+    .modal('show');
+}
+  
 $(document).ready(function() {
 
   // Signout link
